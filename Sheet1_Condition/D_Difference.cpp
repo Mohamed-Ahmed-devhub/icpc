6bcfc9b317c6abe5308da912adeae27152b5f139
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-15 14:00:00 +0200

    feat: solve problems for Mar 15

--- Sheet1_Condition/D_Difference.cpp
+++ Sheet1_Condition/D_Difference.cpp
@@ -6,10 +6,7 @@ int main()
   int x, a, b, c, d;
   cout << "Enter four numbers a, b, c, d :\n";
   cin >> a >> b >> c >> d;
-  if( -100000 <= a && b && c && d <= 100000 )
-  {
     x = (a*b) - (c*d) ;
     cout << "Difference = " << x << "\n";
-  }
   return 0;
 }
