71e8efc70acf97d9e621f409e5c35b09128887ee
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-17 22:36:10 +0200

    add code

---
+++ sheet-1(Data_type-condition)/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout << "Enter tow number :\n";
+  cin >> a >> b;
+  if(0<=a,b<=100)
+  {
+    if(a>=b)
+    {
+      cout << "yes\n";
+    }
+    else
+    cout << "No\n";
+  }
+  return 0;
+}                 
