641481327ec1e7a34a569bc955d73c726fecc093
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-19 01:11:52 +0200

    Solved Problem O: Calculator with conditions

--- Sheet1_Condition/O._Calculator.cpp
+++ Sheet1_Condition/O._Calculator.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
-  
+  int A,B;
+  char S;
+  cin >> A >> S >> B;
+  if(S=='+')
+  {
+    cout << A+B << "\n";
+  }
+  else if(S=='-')
+  {
+    cout << A-B << "\n";
+  }
+  else if(S=='*')
+  {
+    cout << A*B << "\n";
+  }
+  else if(S=='/')
+  {
+    cout << A/B << "\n";
+  }
   return 0;
 }    
