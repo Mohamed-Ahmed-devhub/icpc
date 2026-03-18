bb8382e8e63dfb6dbd901c6c5d7ec233be07a423
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-18 02:27:32 +0200

    feat: solve L - The Brothers in Sheet 1

---
+++ Sheet1_Condition/L_The_Brothers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  string f1,s1,f2,s2;
+  cout << " Enter the first name + the second name :\n";
+  cin >> f1 >> s1 ;
+  cin >> f2 >> s2;
+  if(s1==s2)
+  {
+    cout << "AER BROTHERS\n";
+  }
+  else 
+  cout << "NOT BROTHERS\n";
+  return 0;
+}            
