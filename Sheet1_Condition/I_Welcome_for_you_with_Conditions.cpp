c304d41bc92af55f5c861716502f89d38f87e4c4
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-18 17:41:39 +0200

    Edit cods i-j icpc code force

--- Sheet1_Condition/I_Welcome_for_you_with_Conditions.cpp
+++ Sheet1_Condition/I_Welcome_for_you_with_Conditions.cpp
@@ -1,18 +1,14 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
 using namespace std;
-int main()
-{
-  int a,b;
-  cin >> a >> b;
-  if(0<=a,b<=100)
-  {
-    if(a>=b)
-    {
-      cout << "yes\n";
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a >= b) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;  
     }
-    else
-    cout << "No\n";
-  }
-  return 0;
-}                 
+
+    return 0;
+}
