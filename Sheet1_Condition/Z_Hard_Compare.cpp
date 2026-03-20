d77e4a0c8b36de79333b2e14041cc3d62dfbe92c
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-20 02:24:47 +0200

    Finalized Sheet #1: Mastered Logic, Modulo and Logarithms

--- Sheet1_Condition/Z_Hard_Compare.cpp
+++ Sheet1_Condition/Z_Hard_Compare.cpp
@@ -1,8 +1,18 @@
-include<iostream>
+#include<iostream>
 #include<cmath>
+#include<algorithm>
 using namespace std;
 int main()
 {
-  
+  long long A, B, C, D;
+  cin>> A >> B >> C >>D;
+  double log1=B*log10(A);
+  double log2=D*log10(C);
+  if(log1>log2)
+  {
+  cout <<"YES\n";
+  }
+  else
+  cout <<"NO\n";
   return 0;
 }
