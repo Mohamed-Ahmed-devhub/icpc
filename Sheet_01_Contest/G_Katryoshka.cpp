84203d83f71e9c07d67e1df7dcb1e47295815cf6
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-24 05:42:06 +0200

    add solve code H

--- Sheet_01_Contest/G_Katryoshka.cpp
+++ Sheet_01_Contest/G_Katryoshka.cpp
@@ -2,20 +2,6 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
     
-    int first = N / 10;   
-    int last = N % 10;   
-    if (last == 0) {
-        cout << "YES" << endl;
-    }
-    else if (first % last == 0 || last % first == 0) {
-        cout << "YES" << endl;
-    }
-    else {
-        cout << "NO" << endl;
-    }
-
     return 0;
 }
--- Sheet_01_Contest/H_Data_Type_Guessing.cpp
+++ Sheet_01_Contest/H_Data_Type_Guessing.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main() 
 {
+  long long n,k,a;
+  cin >> n >> k >> a;
+  long long num = n*k;
+  if(num % a!=0)
+  {
+    cout <<"double\n";
+  }
+  else {
+    long long result = num / a;
+    if(result <=2147483647)
+    {
+      cout << "int\n";
+    }
+    else {cout << "long long\n";}
+  }
   
   return 0;
 }
