049a6aa65f38fa90d588cbd1c612f8d81e5696b5
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-26 06:05:15 +0200

    add so;ve code

--- Sheet_01_Contest/G_Katryoshka.cpp
+++ Sheet_01_Contest/G_Katryoshka.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
-    
+    long long n,m,k,total=0;
+    cin >> n >> m >> k;
+    long long res1 = min(n, min(m, k));
+    total+=res1;
+    n-=res1;
+    m-=res1;
+    k-=res1;
+    long long eyes = n/2;
+    long long res2 = min(eyes,k);
+    total += res2;
+    cout << total << endl;
     return 0;
 }
