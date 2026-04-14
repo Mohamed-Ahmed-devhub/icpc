5ad7b7cc038547559bc940b1e8d8ab0dfb772a3b
Mohamed Ahmed <mohamed.ahmed.programer@gmail.com>
2026-04-14 23:10:23 +0200

    edit

--- Sheet_02_Loops/D_ Fixed_Password.cpp
+++ Sheet_02_Loops/D_ Fixed_Password.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() 
-{
-  
+{ 
+  double a,b ,c,d;
+  cin >> a >> b >> c >> d;  
+  double final = (a+b+c+d)/4;  
+  if (final>=50)  
+  {
+    cout << "PASS" << "final " << " = "  << final << endl;
+  }
+  else
+  {
+    cout << "FAIL" << "final " <<  " = " << final << endl;
+  }
   return 0;
 }
