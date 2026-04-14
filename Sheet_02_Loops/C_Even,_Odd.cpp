d7985a2d3333ef8c2314a6c50bfe8fc721ad639c
Mohamed Ahmed <mohamed.ahmed.programer@gmail.com>
2026-04-14 22:52:14 +0200

    add solve  code c

--- Sheet_02_Loops/C_Even,_Odd.cpp
+++ Sheet_02_Loops/C_Even,_Odd.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main() 
 {
-  int 
+  int N,x;
+  cin >> N; 
+  int Even=0,Odd=0,Positive=0,Negative=0;
+  for (int i=0;i<N;i++)
+  {
+    cin >> x;
+    if(x%2==0){
+      Even++;
+    }
+    else{
+      Odd++;
+    }
+    if(x>0){
+      Positive++;
+    }
+    else if(x<0){
+      Negative++;
+    }
+  }
+  cout << "Even: " << Even << endl;
+  cout << "Odd: " << Odd << endl;
+  cout << "Positive: " << Positive << endl;
+  cout << "Negative: " << Negative << endl;
   return 0;
 }
--- Sheet_02_Loops/E_Max.cpp
+++ Sheet_02_Loops/E_Max.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main() 
 {
-  
+  int a, b, c, d;
+  cin >> a >> b >> c >> d ;
+  int max1 = max(a, b);
+  int max2 = max(c, d);
+  int final_max = max(max1, max2);
+  cout << final_max << endl;
+
   return 0;
 }
