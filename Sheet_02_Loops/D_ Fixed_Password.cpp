b3c34860a759fe6cf286524b5600b26ac5a418f7
Mohamed Ahmed <mohamed.ahmed.programer@gmail.com>
2026-04-15 19:02:09 +0200

    Updated some styles

--- Sheet_02_Loops/D_ Fixed_Password.cpp
+++ Sheet_02_Loops/D_ Fixed_Password.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
-int main() 
-{ 
-  
+int main()
+{
+  int pas = 1999;
+  int X;
+  while (true)
+  {
+    cin >> X;
+    if (X == pas)
+    {
+      cout << "Correct" << endl;
+      break;
+    }
+    else
+    {
+      cout << "Wrong" << endl;
+    }
+  }
   return 0;
 }
